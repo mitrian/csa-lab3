68e642aa1f5a55a69d25d95b15b27fd2822b458e
mitrian <onlyvideo01@gmail.com>
2024-08-05 21:45:21 +0300

    feat(datapath.py): method to set flags

--- src/csa_lab3/datapath.py
+++ src/csa_lab3/datapath.py
@@ -4,3 +4,12 @@ class ALU:
 
     min_value: int = -(2 ** 31)
     max_value: int = 2 ** 31 - 1
+
+    def set_flags(self, value: int) -> None:
+        if value > self.max_value:
+            value = value & self.min_value
+
+        if value < self.min_value:
+            value = value & self.min_value
+        self.is_negative = value < 0
+        self.is_zero = value == 0
